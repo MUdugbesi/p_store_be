696916234489af9b773d01377922c9948b73c264
Marvelous <marvelous.udugbesi@icloud.com>
2025-10-04 20:21:50 +0100

    updated index route

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ const authRoutes = require('./routes/auth-route');
 
 app.use('/auth', authRoutes);
 
+app.get('/', (req, res) => {
+	res.json({ message: 'We are live' });
+});
+
 app.listen(3000, (req, res) => {
 	console.log('Listening to port 3000');
 });
